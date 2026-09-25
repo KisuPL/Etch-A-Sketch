b319d200394b5193d014fd7e594e45c992a0d415
kisu <49978920+KisuPL@users.noreply.github.com>
2026-09-25 12:24:59 +0200

    added eraser and rainbow mode

---
+++ scripts.js
@@ -0,0 +1,119 @@
+function clearDrawingArea(){
+    while (gridWrapper.firstChild) {
+        gridWrapper.removeChild(gridWrapper.lastChild);
+    }
+}
+
+function setUpDrawingArea(gridSize = 16) {
+for (let i = 0; i < gridSize; i++){
+    let column = document.createElement("div");
+    column.id = "column";
+    column.style.backgroundColor = "black";
+    gridWrapper.appendChild(column);
+}
+
+    let columnList = document.querySelectorAll("#column");
+    let columnArray = Array.from(columnList);
+    columnArray.forEach(column => {
+        for (let j = 0; j < gridSize; j++){
+            let row = document.createElement("div");
+            row.id = "row";
+            row.style.width=`${drawingAreaSize/gridSize}px`;
+            row.style.height=`${drawingAreaSize/gridSize}px`;
+            row.style.backgroundColor = "white";
+            row.addEventListener("mouseover",() => {
+                row.style.opacity = 0.5;
+            })
+            row.addEventListener("mouseout",() => {
+                row.style.opacity = 1;
+            })
+            row.addEventListener("mouseover",changeColor);
+            column.appendChild(row);
+        }
+    })
+}
+
+function updateGridSize() {
+    rainbow = false
+    eraser = false
+    rainbowButton.classList.remove("btn-on")
+    eraserButton.classList.remove("btn-on")
+    clearDrawingArea()
+    setUpDrawingArea(gridSize.value)
+}
+
+function changeColor(e) {
+e.preventDefault()
+if (!mouseDown) return
+else if (eraser){
+    e.target.style.backgroundColor = "white";
+    return
+} else if (rainbow) {
+    e.target.style.backgroundColor = randomColor();
+    return;
+} else
+e.preventDefault()
+let color = document.getElementById("colorPicker").value
+e.target.style.backgroundColor = color;
+}
+
+function randomColor(){
+        return `hsl(${Math.random() * 360}, 100%, 50%)`;
+}
+
+function rainbowMode(){
+    if (rainbow){
+        rainbow = false
+    } else {
+        rainbow = true
+        if (eraser){
+        eraser = false
+        eraserButton.classList.remove("btn-on")
+        }
+    }
+}
+
+function eraserMode(){
+    if (eraser){
+        eraser = false
+    } else {
+        eraser = true
+        if (rainbow){
+        rainbow = false
+        rainbowButton.classList.remove("btn-on")
+        }
+    }
+}
+
+document.addEventListener("dragstart", e => e.preventDefault());
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+let gridSize = document.getElementById("gridSize");
+
+
+let buttonList = document.querySelectorAll(".toggleable")
+let buttonArray = Array.from(buttonList)
+buttonArray.forEach(button => {
+    button.addEventListener("click",() => {
+        button.classList.toggle("btn-on");
+    })
+})
+
+let clearButton = document.getElementById("clearButton")
+clearButton.addEventListener("click",updateGridSize)
+
+let rainbow = false
+let rainbowButton = document.getElementById("rainbowButton")
+rainbowButton.addEventListener("click",rainbowMode)
+
+let eraser = false
+let eraserButton = document.getElementById("eraserButton")
+eraserButton.addEventListener("click",eraserMode)
+
+let gridWrapper = document.getElementById("gridWrapper");
+let drawingAreaSize = window.getComputedStyle(gridWrapper).getPropertyValue("width").replace("px","");
+
+setUpDrawingArea();
